fdfd91747a115339d28078cec82008820a580cd8
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-05-20 17:40:14 +0400

    Update main.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ while True:
     num_sides = input('Введите число: ')
     if not num_sides.isdigit():
         print("Ошибка вы ввели не число!!!")
-        продолжи
+        КЛЮЧЕВОЕ продолжи
     roll_result = названиефайла.roll_dice(int(num_sides))
     print("You rolled a " + str(roll_result) + "!")
     play_again = input("Roll again? (y/n): ")
     if play_again.lower() != "y":
-        print("Давай отдохнем!")
+       КЛЮЧЕВОЕ перерыв 
 #         сделай рефакторинг согласно линтеру flake8
 #         во втором файле используй randint(1, num_sides)
