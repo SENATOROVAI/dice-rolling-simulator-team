0e9a37485959b3f222212b27abb35e98aae1eabd
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-05-20 17:34:34 +0400

    Update main.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from dice import roll_dice
+import dice 
 
 while True:
-    num_sides = (input('Введите число: '))
+    num_sides = input('Введите число: ')
     if not num_sides.isdigit():
         print("Ошибка вы ввели не число!!!")
-    else:
-        print('Число принято!')
-    roll_result = roll_dice(num_sides=6)
+        продолжи
+    roll_result = названиефайла.roll_dice(int(num_sides))
     print("You rolled a " + str(roll_result) + "!")
     play_again = input("Roll again? (y/n): ")
     if play_again.lower() != "y":
         print("Давай отдохнем!")
+#         сделай рефакторинг согласно линтеру flake8
+#         во втором файле используй randint(1, num_sides)
